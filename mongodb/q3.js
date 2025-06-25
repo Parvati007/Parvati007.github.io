9f5e1a7fe984901a2729bb1edbb426b6d20cad2b
Parvati007 <parvati000777@gmail.com>
2025-06-25 15:36:03 +0530

    updated

--- mongodb/q3.js
+++ mongodb/q3.js
@@ -28,3 +28,22 @@ db.employees.find(
     {salary:{$lte:3000}},
     {_id:0,name:1,salary:1}
 )
+
+db.employees.find(
+    {salary:{$lte:3000},department:"IT"}, //by default AND operator
+    {_id:0,name:1,salary:1}
+)
+
+db.employees.find(
+    {$and:[{salary:{$lte:3000}},{department:"IT"}]},  //and
+    {_id:0,name:1,salary:1}
+)
+
+db.employees.find(
+    {$or:[{salary:{$lte:3000}},{department:"IT"}]},   //or
+    {_id:0,name:1,salary:1}
+)
+
+db.employees.find({department:{$eq:"IT"}})   //equal to
+db.employees.find({department:{$ne:"IT"}})   //not equal to
+
