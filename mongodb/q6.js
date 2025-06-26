ad899ae06e21287fd33895fcec49951e87ebc837
Parvati007 <parvati000777@gmail.com>
2025-06-26 15:40:35 +0530

    updated

--- mongodb/q6.js
+++ mongodb/q6.js
@@ -14,7 +14,7 @@ db.employees.find(
 db.employees.countDocuments()
 
 db.employees.aggregate([
-    {$match:{salary:{$gt:1000}}},
+    {$match:{salary:{$gt:1000}}}, //condition
     {$project:{name:1,salary:1,location:1}},
     {$sort:{name:1}},
     {$limit:1}
@@ -34,4 +34,91 @@ db.employees.aggregate([
 db.employees.aggregate([
     {$project:{_id:0,Empname:"$name"}},
 ])
+
+db.employees.aggregate([
+    {$project:{
+        _id:0,
+        name:1,
+        salary:1,
+        bonus:{$multiply:['$salary',2]}
+    }},
+])
+
+db.employees.aggregate([
+    {$project:{name:0}},
+])
+
+db.employees.aggregate([
+    {$group:{
+        _id:"$department",
+        total:{$sum:"$salary"}
+    }}
+])
+
+db.employees.aggregate([
+    {$group:{
+        _id:null,
+        total:{$sum:"$salary"}
+    }}
+])
+
+
+//create new collections named orders
+//_id:
+//empId:
+//orderValue:
+
+db.orders.insertOne(
+    {
+        empId:ObjectId('685a8a5ca384c5b95c748a5f')
+        ,orderValue:120
+    }
+)
+
+db.orders.insertOne(
+    {
+        empId:ObjectId('685bb9158e58b4721e748a5f')
+        ,orderValue:130
+    }
+)
+
+db.orders.insertOne(
+    {
+        empId:ObjectId('685bb9158e58b4721e748a60')
+        ,orderValue:140
+    }
+)
+
+db.orders.insertOne(
+    {
+        empId:ObjectId('685bc9b78e58b4721e748a65')
+        ,orderValue:150
+    }
+)
+
+db.orders.insertOne(
+    {
+        empId: ObjectId('685bc9b78e58b4721e748a66')
+        ,orderValue:160
+    }
+)
+
+db.employees.aggregate([
+    {$match:{salary:{$gt:2000}}},
+    {$lookup:
+    {
+        from:"orders",
+        localField:"_id",
+        foreignField:"empId",
+        as:"orders",
+    }},
+    {$unwind:"$orders"},
+    {$project:{name:1,salary:1,"orders.orderValue":1}},
+    
+])
+
+
+
+
+
 
