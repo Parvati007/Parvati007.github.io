6e5d211c9d479b3df87f2305341e69ebcaa384d3
Parvati007 <parvati000777@gmail.com>
2025-07-04 16:58:22 +0530

    updated

--- nodejs/nodeapp/index8.js
+++ nodejs/nodeapp/index8.js
@@ -1,9 +1,9 @@
 //npm i bycrypt----------------hasing and bycrypting the password
 
-import bycrypt from "bcrypt";
+import bcrypt from "bcrypt";
 // const pwd = "pass1234"
 // const hashedpwd = await bycrypt.hash(pwd,10);
 // console.log(hashedpwd);
 
-const check = await bycrypt.compare("pass1234","$2b$10$N1cJTLX0yUfBUJwRJrE6ROk67sckTwFOp40MCwgOkOJ8YCST9m582");
+const check = await bcrypt.compare("pass1234","$2b$10$N1cJTLX0yUfBUJwRJrE6ROk67sckTwFOp40MCwgOkOJ8YCST9m582");
 console.log(check);   //true
--- nodejs/nodeapp/index9.js
+++ nodejs/nodeapp/index9.js
@@ -1,6 +1,7 @@
 //npm i jsonwebtoken-----------------used for tokenization
 import express from"express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const app = express();
 const SECRET="sometext";
@@ -8,20 +9,7 @@ const SECRET="sometext";
 app.listen(8080,()=>{
     console.log("Server started");
 })
-const users =[
-    {
-        name:"John",
-        email:"john@gmail.com",
-        password:"1234",
-        role:"user",
-    },
-    {
-        name:"Cathy",
-        email:"cathy@gmail.com",
-        password:"1234",
-        role:"admin",
-    }
-]
+let users =[];
 app.use(express.json());
 
 const authenticate=(req,res,next)=>{
@@ -63,7 +51,16 @@ app.get("/users",authenticate,authorize("admin"),(req,res)=>{
     res.json(users);
 })
 
-//token = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiIsImVtYWlsI
-//joiam9obkBnbWFpbC5jb20iLCJwYXNzd29yZCI6IjEyMzQiLCJyb2xlIjoidXNlciIsImlhdCI6MT
-// c1MTYyNDQwMn0.EKh7YSsyxJrwp3_NshD5afoH6C0zbFW9EuXx2NS_6Ts
- 
+
+app.post("/register",(req,res)=>{ 
+  const {name,email,password,role}=req.body;
+  const hashedpwd =  bcrypt.hash(password,10);
+  const user = {
+    name,
+    email,
+    password: hashedpwd,
+    role,
+  }                                               
+  users.push(req.body);
+  res.json(user);
+})
