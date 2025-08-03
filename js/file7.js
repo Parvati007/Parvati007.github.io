0b1b5935f143fad7aa279ec24f36b0e17dfb4acd
Parvati007 <parvati000777@gmail.com>
2025-08-03 23:00:11 +0530

    updated

--- js/file7.js
+++ js/file7.js
@@ -9,7 +9,7 @@ main(f1); //function as arguments...
 //-----------------------------------------------------------------------------------------------
 
 const add = (...args)=>{
-    console.log(args)                  //arrow function we use spred operator(...) for arguments
+    console.log(args)          //arrow function we use spred operator(...) for arguments
     console.log(args[0])
 }
 add(4,5,6,7,8)
